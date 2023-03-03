3f08d42ec70e26991db1634f8afca591e3899cf7
Katažina <katazina.fukalova@gmail.com>
2023-03-03 13:31:04 +0200

    update

--- homework/08-console.js
+++ homework/08-console.js
@@ -1,12 +1,18 @@
 //8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
 
-function lygineSuma(a,b){
-
-    if (Array.isArray(a) == false && Array.isArray(b) == false) {
-        return `Pateikta tinkama reikšmė abu masyvai.`;
-    } 
-    else if (typeof number !== a || typeof number !== b ) {
-        return `Pateika tinkama reikšmė abu skaiciai.`;
+function lygineSuma(a, b) {
+    if ((typeof a !== 'number' && !Array.isArray(a)) || (typeof b !== 'number' && !Array.isArray(b))) {
+    return 'Error';
+    }let sum = 0;if (typeof a === 'number' && typeof b === 'number') {
+    sum = a + b;
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+    sum = a.length + b.length;
+    } else {
+    return 'Suma nelyginė';
     }
-   else  return 'Blogas duomenu tipas';
-}
+    if (sum % 2 !== 0) { 
+    return 'Suma nelyginė';
+    } else {
+    return sum;
+    }
+    }
